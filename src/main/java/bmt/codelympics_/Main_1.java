4c827545736c4169d1c0e3de15a13ecbba482c78
Alexis1513 <ale15siri@gmail.com>
2024-09-08 12:37:27 +0200

    debug

--- src/main/java/bmt/codelympics_/Main_1.java
+++ src/main/java/bmt/codelympics_/Main_1.java
@@ -1,7 +1,9 @@
 //Il programma appartiene al package bmt.codelympics_
 package bmt.codelympics_;
+
 //Importa la classe Index per usarla
 import bmt.codelympics_.model.Index;
+
 //La classe Main_1 contiene il metodo main che avvia l'esecuzione chiamando Index.main(args)
 public class Main_1 {
     public static void main(String[] args) {
